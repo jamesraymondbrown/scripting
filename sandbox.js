110498bfc275af7039c8339f622485badfab0a3e
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-03 12:39:22 -0800

    Add files for typescript practice

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = "snail";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
