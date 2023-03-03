77cd95c6ad7726839a9dcb443eb5437ca18c0804
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-03 15:30:54 -0800

    add variables for each input on the screen

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-const anchor = document.querySelector("a");
+const anchor = document.querySelector("a")!;
 
-if (anchor) {
-  console.log(anchor.href);
-}
+console.log(anchor.href);
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
