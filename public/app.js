b3243d9b2fa6a5f8511e097cc65616f91421417a
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-04 13:56:13 -0800

    add ul render logic to app.ts

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 const me = {
     name: "Jim",
@@ -15,19 +16,15 @@ const greetPerson = (person) => {
     console.log("hello ", person.name);
 };
 greetPerson(me);
-// const invOne = new Invoice("Joe", "grass cutting", 50);
-// const invTwo = new Invoice("Jim", "grass growing", 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne, invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.format());
-// });
 // inputs
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -37,5 +34,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,7 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("Yoshi", "web work", 250);
-// docTwo = new Payment("Mario", "plumbing work", 200);
-
-// const docs: HasFormatter[] = [];
-// docs.push(docOne, docTwo);
-
-// console.log(docs);
 
 // interfaces
 interface IsPerson {
@@ -39,22 +29,16 @@ const greetPerson = (person: IsPerson) => {
 
 greetPerson(me);
 
-// const invOne = new Invoice("Joe", "grass cutting", 50);
-// const invTwo = new Invoice("Jim", "grass growing", 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne, invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.format());
-// });
-
 // inputs
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -66,5 +50,5 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
