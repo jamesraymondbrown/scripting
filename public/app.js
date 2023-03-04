ee31178b3fa05d10f4c8570a5c7c2d7a8a9c708a
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-04 14:29:53 -0800

    change submit action to use a tuple

--- public/app.js
+++ public/app.js
@@ -27,12 +27,17 @@ const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values = [
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber,
+    ];
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "end");
 });
